aef1b5972347f5b841e777789c40be44d62efb19
akhand <akhandiiit@gmail.com>
2020-05-13 11:00:52 +0530

    Updates

---
+++ precache-manifest.3a60e9f8ec9c2c96680374b88a9496b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbba9eb56b480ba6e76c1c8335d5518e",
+    "url": "/covid19tracker/index.html"
+  },
+  {
+    "revision": "c4b6b5bf031c086b4d3e",
+    "url": "/covid19tracker/static/css/main.d3c8fc7f.chunk.css"
+  },
+  {
+    "revision": "af6704112cb5c6c4b921",
+    "url": "/covid19tracker/static/js/2.480b2e26.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19tracker/static/js/2.480b2e26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4b6b5bf031c086b4d3e",
+    "url": "/covid19tracker/static/js/main.fa4f8790.chunk.js"
+  },
+  {
+    "revision": "e246b68359dcb8d63456",
+    "url": "/covid19tracker/static/js/runtime-main.77f79652.js"
+  },
+  {
+    "revision": "c1633e998d411c9034b9632fd04d4d2a",
+    "url": "/covid19tracker/static/media/covid19_logo.c1633e99.png"
+  }
+]);
